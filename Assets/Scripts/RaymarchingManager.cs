efc40916aba0659b178d0c999355a9c8a5e66d1b
DanTGL <33489389+DanTGL@users.noreply.github.com>
2020-11-06 16:04:58 +0100

    Added compute shader

---
+++ Assets/Scripts/RaymarchingManager.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[ImageEffectAllowedInSceneView]
+public class RaymarchingManager : MonoBehaviour {
+
+    public ComputeShader computeShader;
+
+    private RenderTexture target;
+
+    private Camera cam;
+
+    private int raymarchKernel;
+
+    void Start() {
+        cam = GetComponent<Camera>();
+        if (computeShader != null) {
+            raymarchKernel = computeShader.FindKernel("CSMain");
+        }
+    }
+
+    void OnRenderImage(RenderTexture source, RenderTexture dest) {
+        if (computeShader == null) {
+            Graphics.Blit(source, dest);
+            return;
+        }
+
+        if (target != null) {
+            target.Release();
+        }
+
+        target = new RenderTexture(cam.pixelWidth, cam.pixelHeight, 0);
+        target.enableRandomWrite = true;
+        target.Create();
+
+        computeShader.SetTexture(raymarchKernel, "Result", target);
+        computeShader.Dispatch(raymarchKernel, cam.pixelWidth / 8, cam.pixelHeight / 8, 1);
+
+        Graphics.Blit(target, dest);
+    }
+
+}
---
+++ Assets/Scripts/RaymarchingObject.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RaymarchingObject : MonoBehaviour {
+
+    enum ShapeType {
+        NONE,
+        SPHERE,
+        CUBE
+    }
+
+    [SerializeField]
+    private ShapeType shape = ShapeType.NONE;
+
+    [SerializeField]
+    private float radius = 1f;
+
+    [SerializeField]
+    private Color surfaceColor = Color.white;
+
+    public Vector3 GetPosition() {
+        return transform.position;
+    }
+
+    public int GetShape() {
+        return (int) shape;
+    }
+
+    public float GetRadius() {
+        return radius;
+    }
+
+    public Color GetSurfaceColor() {
+        return surfaceColor;
+    }
+
+    public Vector3 GetScale() {
+        return transform.localScale;
+    }
+
+}
