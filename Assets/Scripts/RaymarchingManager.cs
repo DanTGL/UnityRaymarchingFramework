536a3f219412220af44e975ff79fe494f4598ca1
DanTGL <33489389+DanTGL@users.noreply.github.com>
2020-11-10 13:50:04 +0100

    Added raymarch functionality to compute shader

--- Assets/Scripts/RaymarchingManager.cs
+++ Assets/Scripts/RaymarchingManager.cs
@@ -1,9 +1,7 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using UnityEngine;
-using UnityEditor;
 
-[ImageEffectAllowedInSceneView]
+[ExecuteInEditMode, ImageEffectAllowedInSceneView]
 public class RaymarchingManager : MonoBehaviour {
 
     public ComputeShader computeShader;
@@ -14,31 +12,78 @@ public class RaymarchingManager : MonoBehaviour {
 
     private int raymarchKernel;
 
+    struct RaymarchObject {
+        public int shape;
+        public float radius;
+        public Vector3 position;
+        public Vector3 scale;
+        public Color surfaceColor;
+    }
+
+    private List<RaymarchObject> objects;
+
     void Start() {
         cam = GetComponent<Camera>();
         if (computeShader != null) {
             raymarchKernel = computeShader.FindKernel("CSMain");
         }
+
+        FindObjects();
     }
 
+    void FindObjects() {
+        objects = new List<RaymarchObject>();
+        RaymarchingObject[] foundObjs = FindObjectsOfType<RaymarchingObject>();
+        for (int i = 0; i < foundObjs.Length; i++) {
+            RaymarchObject obj;
+            obj.shape = foundObjs[i].GetShape();
+            obj.radius = foundObjs[i].GetRadius();
+            obj.position = foundObjs[i].GetPosition();
+            obj.scale = foundObjs[i].GetScale();
+            obj.surfaceColor = foundObjs[i].GetSurfaceColor();
+            objects.Add(obj);
+        }
+        
+        
+    }
+
+    [ImageEffectOpaque]
     void OnRenderImage(RenderTexture source, RenderTexture dest) {
         if (computeShader == null) {
             Graphics.Blit(source, dest);
             return;
         }
+        
+        InitRenderTexture();
 
-        if (target != null) {
-            target.Release();
-        }
+        FindObjects();
 
-        target = new RenderTexture(cam.pixelWidth, cam.pixelHeight, 0);
-        target.enableRandomWrite = true;
-        target.Create();
+        ComputeBuffer buffer = new ComputeBuffer(objects.Count, 48);
+        buffer.SetData(objects.ToArray());
 
+        computeShader.SetMatrix("_CameraToWorld", cam.cameraToWorldMatrix);
+        computeShader.SetMatrix("_CameraInverseProjection", cam.projectionMatrix.inverse);
+        computeShader.SetInt("_numShapes", objects.Count);
+        computeShader.SetBuffer(raymarchKernel, "dataBuffer", buffer);
+        //computeShader.SetTexture(raymarchKernel, "_SkyboxTexture", RenderSettings.skybox.mainTexture);
         computeShader.SetTexture(raymarchKernel, "Result", target);
         computeShader.Dispatch(raymarchKernel, cam.pixelWidth / 8, cam.pixelHeight / 8, 1);
+        buffer.Release();
 
         Graphics.Blit(target, dest);
+    }
+
+    void InitRenderTexture() {
+        if (target == null || target.width != Screen.width || target.height != Screen.height) {
+
+            if (target != null) {
+                target.Release();
+            }
+
+            target = new RenderTexture(Screen.width, Screen.height, 0, RenderTextureFormat.ARGBFloat, RenderTextureReadWrite.Linear);
+            target.enableRandomWrite = true;
+            target.Create();
+        }
     }
 
 }
